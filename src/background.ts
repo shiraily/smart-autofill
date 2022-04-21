e78ce1f7e5e249538715f5c3e4ccb3b1a82808c8
Takaaki Shirai <25303121+shiraily@users.noreply.github.com>
2022-04-22 01:35:20 +0900

    refactor

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,7 @@
 import browser from "webextension-polyfill";
 
-// FIXME: use try as work around to debug under Chrome 93
-try {
-	console.log("background is loaded!");
-
-	browser.action.onClicked.addListener(function(tab: any) {
-		browser.tabs.sendMessage(tab.id, "FillSignUp").catch(e => {
-			console.error("Err!", e);
-		});
+browser.action.onClicked.addListener(function (tab: any) {
+  browser.tabs.sendMessage(tab.id, "FillSignUp").catch((e) => {
+    console.error("Err!", e);
   });
-} catch(e) {
-	console.log("err", e);
-}
+});
